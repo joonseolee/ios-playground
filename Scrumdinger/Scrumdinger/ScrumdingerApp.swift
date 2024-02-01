5ac1133b3078fd93e34ee372707693f824fc50f3
이준서(Joonseo Lee)/Bot솔루션개발팀/SKP <joonseo.lee@sk.com>
2024-02-01 09:43:15 +0900

    first commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 이준서(Joonseo Lee)/Bot솔루션개발팀/SKP on 2024/02/01.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
